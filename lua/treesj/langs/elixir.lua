694b59e7f4c01a61b47ea37ea59d124d82b8fa7c
David Nelson <davidicus21@gmail.com>
2024-06-11 14:19:16 -0600

    copy ruby, implement list and map

---
+++ lua/treesj/langs/elixir.lua
@@ -0,0 +1,218 @@
+local lang_utils = require('treesj.langs.utils')
+local utils = require('treesj.utils')
+
+return {
+  list = lang_utils.set_preset_for_list(),
+  map_content = lang_utils.set_preset_for_list({
+    split = {
+      last_separator = false,
+      non_bracket_node = true,
+    }
+  }),
+  method_parameters = lang_utils.set_preset_for_args(),
+  argument_list = lang_utils.set_preset_for_args({
+    both = {
+      enable = function(tsn)
+        return tsn:parent():type() ~= 'return'
+      end,
+    },
+  }),
+  block = lang_utils.set_preset_for_dict({
+    join = {
+      separator = '',
+      recursive = false,
+      format_tree = function(tsj)
+        if tsj:has_children({ 'do', 'end' }) then
+          tsj:child('do'):update_text('{')
+          tsj:child('end'):update_text('}')
+        end
+      end,
+    },
+    split = {
+      omit = { 'block_parameters' },
+      separator = '',
+      format_tree = function(tsj)
+        if tsj:has_children({ '{', '}' }) then
+          tsj:child('{'):update_text('do')
+          tsj:child('}'):update_text('end')
+        end
+      end,
+    },
+  }),
+  string_array = lang_utils.set_preset_for_list({
+    split = {
+      last_separator = false,
+    },
+  }),
+  body_statement = lang_utils.set_preset_for_non_bracket({
+    join = {
+      force_insert = ';',
+      no_insert_if = {
+        lang_utils.helpers.if_penultimate,
+      },
+    },
+  }),
+  if_modifier = lang_utils.set_default_preset({
+    join = { enable = false },
+    split = {
+      omit = { lang_utils.helpers.if_second },
+      format_tree = function(tsj)
+        local if_node = tsj:child('if')
+        local end_ = tsj:create_child({ text = 'end', type = 'end' })
+        tsj:update_children({ if_node, if_node:next(), tsj:child(1), end_ })
+      end,
+    },
+  }),
+  unless_modifier = lang_utils.set_default_preset({
+    join = { enable = false },
+    split = {
+      omit = { lang_utils.helpers.if_second },
+      format_tree = function(tsj)
+        local if_node = tsj:child('unless')
+        local end_ = tsj:create_child({ text = 'end', type = 'end' })
+        tsj:update_children({ if_node, if_node:next(), tsj:child(1), end_ })
+      end,
+    },
+  }),
+  ['if'] = lang_utils.set_default_preset({
+    split = { enable = false },
+    join = {
+      enable = function(tsn)
+        local check = {
+          tsn:field('consequence')[1],
+          tsn:field('alternative')[1],
+        }
+
+        return utils.every(check, function(el)
+          return not el and true or el:named_child_count() == 1
+        end)
+      end,
+      space_in_brackets = true,
+      format_tree = function(tsj)
+        if tsj:child('else') then
+          local if_ = tsj:child('if')
+          local else_ = tsj:child('else')
+          if else_:has_to_format() then
+            local text = else_:child(1):text()
+            else_:child(1):update_text(text:gsub('^else', ':'))
+          else
+            local text = else_:text()
+            else_:update_text(text:gsub('^else', ':'))
+          end
+          if_:update_text('? ')
+          tsj:update_children({ tsj:child(2), if_, tsj:child('then'), else_ })
+        else
+          local if_node = tsj:child('if')
+          tsj:update_children({ tsj:child('then'), if_node, if_node:next() })
+        end
+      end,
+    },
+  }),
+  ['unless'] = lang_utils.set_default_preset({
+    split = { enable = false },
+    join = {
+      enable = function(tsn)
+        local check = {
+          tsn:field('consequence')[1],
+          tsn:field('alternative')[1],
+        }
+
+        return utils.every(check, function(el)
+          return not el and true or el:named_child_count() == 1
+        end)
+      end,
+      space_in_brackets = true,
+      format_tree = function(tsj)
+        if tsj:child('else') then
+          local unless_ = tsj:child('unless')
+          local else_ = tsj:child('else')
+          if else_:has_to_format() then
+            local text = else_:child(1):text()
+            else_:child(1):update_text(text:gsub('^else', '?'))
+          else
+            local text = else_:text()
+            else_:update_text(text:gsub('^else', '?'))
+          end
+          unless_:update_text(': ')
+          tsj:update_children({ tsj:child(2), else_, unless_, tsj:child('then') })
+        else
+          local unless_node = tsj:child('unless')
+          tsj:update_children({ tsj:child('then'), unless_node, unless_node:next() })
+        end
+      end,
+    },
+  }),
+  conditional = lang_utils.set_default_preset({
+    join = { enable = false },
+    split = {
+      omit = { lang_utils.helpers.if_second },
+      format_tree = function(tsj)
+        tsj:create_child({ text = 'end', type = 'end' }, #tsj:children() + 1)
+        tsj:child('?'):update_text('if ')
+        tsj:child(':'):update_text('else')
+        tsj:swap_children(1, 2)
+      end,
+      format_resulted_lines = function(lines)
+        return vim.tbl_map(function(line)
+          if line:match('%s.else$') then
+            -- TODO: create helper `unindent`
+            local rgx = '^' .. (' '):rep(vim.fn.shiftwidth())
+            return line:gsub(rgx, '')
+          else
+            return line
+          end
+        end, lines)
+      end,
+    },
+  }),
+  when = lang_utils.set_default_preset({
+    both = {
+      omit = { lang_utils.helpers.if_second },
+    },
+    join = {
+      space_in_brackets = true,
+      enable = function(tsn)
+        return tsn:field('body')[1]:named_child_count() == 1
+      end,
+      format_tree = function(tsj)
+        tsj:create_child({ type = 'then', text = 'then' }, -1)
+      end,
+    },
+    split = {
+      last_indent = 'inner',
+      format_tree = function(tsj)
+        if tsj:has_children({ 'then' }) then
+          local text = tsj:child('then'):text()
+          tsj:child('then'):update_text(text:gsub('then%s', ''))
+        end
+      end,
+    },
+  }),
+  right = lang_utils.set_default_preset({
+    both = {
+      enable = function(tsn)
+        return not (tsn:type() == 'begin' and tsn:named_child_count() > 1)
+      end,
+    },
+    split = {
+      format_tree = function(tsj)
+        tsj:wrap({ left = 'begin', right = 'end' })
+      end,
+    },
+    join = {
+      format_tree = function(tsj)
+        tsj:remove_child({ 'begin', 'end' })
+      end,
+    },
+  }),
+  operator_assignment = { target_nodes = { 'right' } },
+  method = {
+    target_nodes = { 'body_statement' },
+  },
+  assignment = {
+    target_nodes = { 'array', 'hash' },
+  },
+  call = {
+    target_nodes = { 'block', 'do_block' },
+  },
+}
---
+++ tests/sample/index.ex
@@ -0,0 +1,135 @@
+# RESULT OF JOIN (node "list", preset default)
+list = [ 1, 2, 3, [ 1, 2 ], %{:bar => 1} ]
+
+# RESULT OF SPLIT (node "list", preset default)
+list = [
+  1,
+  2,
+  3,
+  [ 1, 2 ],
+  %{:bar => 1},
+]
+
+# RESULT OF JOIN (node "map", preset default)
+map = %{ :foo => "foo", :bar => 'bar', [ 1, 2 ] => 2 }
+
+# RESULT OF SPLIT (node "map", preset default)
+map = %{
+  :foo => "foo",
+  :bar => 'bar',
+  [ 1, 2 ] => 2
+}
+
+# RESULT OF JOIN (node "method_parameters", preset default)
+def test(a, b) do end
+
+# RESULT OF SPLIT (node "method_parameters", preset default)
+def test(
+  a,
+  b
+) do end
+
+# RESULT OF JOIN (node "argument_list", preset default)
+test(a, b)
+
+# RESULT OF JOIN (node "argument_list", preset default)
+test(
+  a,
+  b
+)
+
+# RESULT OF JOIN (node "word_list", preset default)
+%w( foo bar baz )
+
+# RESULT OF split (node "word_list", preset default)
+%w(
+  foo
+  bar
+  baz
+)
+
+# RESULT OF JOIN (node "body_statement", preset default)
+def double(x), do: IO.inspect("func");  x * 2
+
+# RESULT OF JOIN (node "body_statement", preset default)
+  def double(x) 
+  IO.inspect("meth")
+  x * 2
+end
+
+# RESULT OF JOIN (node "do_block to block", preset default)
+x.each { |y| puts y }
+
+# RESULT OF SPLIT (node "block to do_block", preset default)
+x.each do |y|
+  puts y
+end
+
+# RESULT OF JOIN (node "if_modifier to if", preset default)
+return false if true
+
+# RESULT OF SPLIT (node "if to if_modifier", preset default)
+if true
+  return false
+end
+
+# RESULT OF JOIN (node "conditional to if", preset default)
+cond ? do_that('cond') : do_this('not nond')
+
+# RESULT OF SPLIT (node "if to conditional", preset default)
+if cond
+  do_that('cond')
+else
+  do_this('not nond')
+end
+
+# RESULT OF JOIN (node "conditional to if", preset default)
+cond ? do_that('cond') : do_this('not nond')
+
+# RESULT OF SPLIT (node "if to conditional", preset default)
+if cond
+  do_that('cond')
+else
+  do_this('not nond')
+end
+
+# RESULT OF JOIN (node "when", preset default)
+case
+when true then print('false')
+end
+
+# RESULT OF SPLIT (node "when", preset default)
+case
+when true
+  print('false')
+end
+
+# RESULT OF JOIN (field "right" inside "operator_assignment", preset default)
+def config
+  @config ||= method
+end
+
+# RESULT OF SPLIT (field "right" inside "operator_assignment", preset default)
+def config
+  @config ||= begin
+    method
+  end
+end
+
+# RESULT OF JOIN (node "unless_modifier to unless", preset default)
+return false unless true
+
+# RESULT OF SPLIT (node "unless to unless_modifier", preset default)
+unless true
+  return false
+end
+
+# RESULT OF JOIN (node "conditional to unless", preset default)
+cond ? do_that('cond') : do_this('not nond')
+
+# RESULT OF SPLIT (node "unless to conditional", preset default)
+unless cond
+  do_this('not nond')
+else
+  do_that('cond')
+end
