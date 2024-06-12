537473f27c0254e77b081b1d123fbc4660c47214
David Nelson <davidicus21@gmail.com>
2024-06-12 16:44:58 -0600

    determine function def

--- lua/treesj/langs/elixir.lua
+++ lua/treesj/langs/elixir.lua
@@ -1,6 +1,18 @@
 local lang_utils = require('treesj.langs.utils')
 local utils = require('treesj.utils')
 
+local query = require('vim.treesitter.query')
+local ts = require('vim.treesitter')
+
+local function is_function_def(tsj)
+  local prev = tsj:root():tsnode():prev_sibling()
+  if prev == nil then
+    return false
+  end
+  local text = ts.get_node_text(prev, 0)
+  return text == "def" or text == "defp"
+end
+
 return {
   list = lang_utils.set_preset_for_list(),
   map_content = lang_utils.set_preset_for_list({
@@ -10,7 +22,11 @@ return {
     }
   }),
   map = { target_nodes = { 'map_content' } },
-  arguments = lang_utils.set_preset_for_args(),
+  arguments = lang_utils.set_preset_for_args({
+    format_tree = function(tsj)
+      print(vim.inspect(is_function_def(tsj)))
+    end
+  }),
   quoted_content = lang_utils.set_preset_for_list({
     split = {
       format_tree = function(tsj)
@@ -27,6 +43,38 @@ return {
   }),
   sigil = {
     target_nodes = { 'quoted_content' }
+  },
+  -- identifier = {
+  --   split = {
+  --     format_tree = function(tsj)
+  --       print(vim.inspect(tsj:text()))
+  --       print(vim.inspect("next"))
+  --       local next_sib = tsj:tsnode():next_sibling()
+  --       print(vim.inspect(utils.get_node_text(next_sib)))
+  --       for i, c in pairs(tsj:next():children()) do
+  --         print("********************")
+  --         print(c:text())
+  --       end
+  --     end
+  --   }
+  -- },
+  -- keyword = {
+  --   split = {
+  --     format_tree = function(tsj)
+  --       print(vim.inspect(tsj:text()))
+  --       for i, c in pairs(tsj:children()) do
+  --         print(c:text())
+  --       end
+  --     end
+  --   }
+  -- }
+  call = {
+    -- target_nodes = { 'arguments' }
+    split = {
+      format_tree = function(tsj)
+        print(vim.inspect(is_function_def(tsj)))
+      end,
+    }
   }
 
 }
